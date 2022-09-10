2caca94952f4af10434bfe69ecd625fe18278ec3
pranavdeshmukh1999 <pranavdeshmukh182@gmail.com>
2022-09-11 02:03:16 +0530

    Added : Num is ODD-EVEN

--- src/com/OddEven/OddEvenAssignment.java
+++ src/com/OddEven/OddEvenAssignment.java
@@ -1,10 +1,21 @@
 package com.OddEven;
 
+import java.util.Scanner;
+
 public class OddEvenAssignment {
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcom to Odd-Even Program");
-	}
 
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter a number: ");
+		int num = scanner.nextInt();
+
+		if (num % 2 == 0)
+			System.out.println(num + " is even");
+		else
+			System.out.println(num + " is odd");
+	}
 }
